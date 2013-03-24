f2c25df0499c23574cb18940bb9ff47a3f07385d
fogus <mefogus@gmail.com>
2013-03-24 10:16:10 -0400

    Trimming deps

--- project.clj
+++ project.clj
@@ -3,14 +3,13 @@
   :url "http://www.fogus.me/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-swank "1.4.4"]
-            [lein-cljsbuild "0.2.9"]
+  :plugins [[lein-cljsbuild "0.2.9"]
             [lein-marginalia "0.7.1"]
             [lein-multi "1.1.0"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.0"]]
                "1.3" [[org.clojure/clojure "1.3.0"]]
                "1.4" [[org.clojure/clojure "1.4.0"]]
-               "1.5" [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+               "1.5" [[org.clojure/clojure "1.5.0"]]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :extra-classpath-dirs ["checkouts/clojurescript/src/clj"
                          "checkouts/clojurescript/src/cljs"]
