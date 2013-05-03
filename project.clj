b4ba3d2d14bec808419b78b7b261cd4c7bc53782
fogus <mefogus@gmail.com>
2013-05-03 13:55:14 -0400

    Moving to Lein profiles

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://www.fogus.me/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-cljsbuild "0.2.9"]
-            [lein-marginalia "0.7.1"]
-            [lein-multi "1.1.0"]]
+            [lein-marginalia "0.7.1"]]
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
@@ -17,6 +17,5 @@
   :cljsbuild {:builds [{:source-path "src/clojurescript"
                         :compiler {:output-to "resources/js/bacwn.none.js"
                                    :output-dir "resources/js/"
-                                   :optimizations :none}}]}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+                                   :optimizations :none}}]})
 
