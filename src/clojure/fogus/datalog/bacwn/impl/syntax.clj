ed1eb5fc30d44b5305772ca284ee46f60159c391
fogus <mefogus@gmail.com>
2013-05-22 13:58:34 -0400

    Separating syntactic elements

--- src/clojure/fogus/datalog/bacwn/impl/syntax.clj
+++ src/clojure/fogus/datalog/bacwn/impl/syntax.clj
@@ -13,4 +13,22 @@
 
 (ns fogus.datalog.bacwn.impl.syntax)
 
+(def ID_KEY :db.id)
+(def ^:private nums (atom 0))
+
+(defn- explode
+  "Convert a map into a clj-Datalog tuple vector. Brittle, but
+   works along the happy path."
+  [entity]
+  (let [relation-type (-> entity seq ffirst namespace keyword)
+        id-key (keyword (name relation-type) "db.id")
+        id  (get entity id-key)
+        id  (if id id (swap! nums inc))
+        kvs (seq (dissoc entity id-key))]
+    (vec
+     (apply concat [relation-type :db.id id]
+            (reduce (fn [acc [k v]]
+                      (cons [(keyword (name k)) v] acc))
+                    []
+                    kvs)))))
 
