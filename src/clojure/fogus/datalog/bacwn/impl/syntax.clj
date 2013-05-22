92982c11141e91bda85e23abef7e3e65701701b9
fogus <mefogus@gmail.com>
2013-05-22 13:58:08 -0400

    Separating syntactic elements

--- src/clojure/fogus/datalog/bacwn/impl/syntax.clj
+++ src/clojure/fogus/datalog/bacwn/impl/syntax.clj
@@ -1,3 +1,16 @@
-(ns fogus.datalog.bacwn.impl.syntax
-  (:require [fogus.datalog.bacwn.impl.util :as util]
-            clojure.set))
+;;  Copyright (c) Michael Fogus. All rights reserved.  The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;  be found in the file epl-v10.html at the root of this distribution.  By
+;;  using this software in any fashion, you are agreeing to be bound by the
+;;  terms of this license.  You must not remove this notice, or any other,
+;;  from this software.
+;;
+;;  syntax.clj
+;;
+;;  A Clojure implementation of Datalog -- Support for in-memory database
+;;
+
+(ns fogus.datalog.bacwn.impl.syntax)
+
+
